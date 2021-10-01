653ffb493938bf0980051169281c327d06719ff9
kgandhi09 <kushal009gandhi@gmail.com>
2021-09-30 23:53:37 -0400

    leanred how to swap the back buffer with the front buffer

--- src/collision_detection.cpp
+++ src/collision_detection.cpp
@@ -23,7 +23,21 @@ int main() {
 	//makes the OpenGL context of specified window current on the calling thread
 	glfwMakeContextCurrent(window);
 
+	//Load GLAD so it configures OpenGL
+	gladLoadGL();
+
+	//Specify the ViewPort of OpenGL in the window
+	glViewport(0,0,800,800);
+
+	//Specify the color of the window background
+	glClearColor(0.27f, 0.05f, 0.34f, 1.0f);
+	//Clean the back buffer and assign the new color to it
+	glClear(GL_COLOR_BUFFER_BIT);
+	//Swap the back buffer with the front buffer
+	glfwSwapBuffers(window);
+
 	while(!glfwWindowShouldClose(window)){
+		//Take care of all GLFW events
 		glfwPollEvents();
 	}
 
